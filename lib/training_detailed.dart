04665d90cb7e4217e28ccdb91e8fe82b09c47e96
NasmiyNahim <nasmiyanahim@gmail.com>
2024-04-22 22:07:40 +0530

    p commit

--- lib/training_detailed.dart
+++ lib/training_detailed.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class TrainingDetailed extends StatelessWidget {
-  const TrainingDetailed ({Key? key});
+  const TrainingDetailed({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,14 +26,13 @@ class TrainingDetailed extends StatelessWidget {
           ),
         ),
       ),
-      
       body: SingleChildScrollView(
         child: Center(
           child: Padding(
             padding: const EdgeInsets.only(top: 20.0), // Adjust the top padding as needed
             child: Container(
               width: 325,
-              height: 750,
+              height: 700,
               decoration: ShapeDecoration(
                 color: Colors.white,
                 shape: RoundedRectangleBorder(
@@ -126,12 +125,384 @@ class TrainingDetailed extends StatelessWidget {
                       ],
                     ),
                   ),
-                  // Add your other content here
+                  Positioned(
+                    left: 5,
+                    right: 5,
+                    top: 80, // Adjust the top position as needed
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0), // Adjust padding as needed
+                      child: Container(
+                        child: Divider(
+                          height: 1.0,
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 85, // Adjust the left position as needed
+                    top: 85, // Adjust the top position as needed
+                    child: Text(
+                      'Dr. Subhash',
+                      style: TextStyle(
+                        color: Color(0xFF8B8B8B),
+                        fontSize: 12,
+                        fontFamily: 'Nunito',
+                        fontWeight: FontWeight.w700,
+                        height: 0,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 158, // Adjust the left position as needed
+                    top: 85, // Adjust the top position as needed
+                    child: Text(
+                      '||',
+                      style: TextStyle(
+                        color: Color(0xFF8B8B8B),
+                        fontSize: 12,
+                        fontFamily: 'Nunito',
+                        fontWeight: FontWeight.w700,
+                        height: 0,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 172, // Adjust the left position as needed
+                    top: 85, // Adjust the top position as needed
+                    child: Text(
+                      'IIT Madras',
+                      style: TextStyle(
+                        color: Color(0xFF8B8B8B),
+                        fontSize: 12,
+                        fontFamily: 'Nunito',
+                        fontWeight: FontWeight.w700,
+                        height: 0,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 85, // Adjust the left position as needed
+                    top: 110, // Adjust the top position as needed
+                    child: Opacity(
+                      opacity: 0.80,
+                      child: Container(
+                        width: 70,
+                        height: 25,
+                        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                        clipBehavior: Clip.antiAlias,
+                        decoration: ShapeDecoration(
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(width: 1, color: Color(0xFF414ECA)),
+                            borderRadius: BorderRadius.circular(7),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'On-Site',
+                              style: TextStyle(
+                                color: Color(0xFF1B94F6),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 162, // Adjust the left position as needed
+                    top: 114, // Adjust the top position as needed
+                    child: Text(
+                      '07 Mar 2024; Thursday',
+                      style: TextStyle(
+                        color: Color(0xFFF31919),
+                        fontSize: 10,
+                        fontFamily: 'Nunito',
+                        fontWeight: FontWeight.w700,
+                        height: 0,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 85, // Adjust the left position as needed
+                    top: 140, // Adjust the top position as needed
+                    child: Opacity(
+                      opacity: 0.80,
+                      child: Container(
+                        width: 176,
+                        height: 22,
+                        padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 4),
+                        clipBehavior: Clip.antiAlias,
+                        decoration: ShapeDecoration(
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(width: 1, color: Color(0xFFEE5602)),
+                            borderRadius: BorderRadius.circular(7),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'ShareInfo for Learn Assessment',
+                              style: TextStyle(
+                                color: Color(0xFFEE5602),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 5,
+                    right: 5,
+                    top: 175, // Adjust the top position as needed
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0), // Adjust padding as needed
+                      child: Container(
+                        child: Divider(
+                          height: 1.0,
+                          color: Color(0xFFD9D9D9),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 17, // Adjust the left position as needed
+                    top: 187, // Adjust the top position as needed
+                    child: SizedBox(
+                      width: 288,
+                      child: Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                              text: '1. Introduction to Python\n',
+                              style: TextStyle(
+                                color: Color(0xFF414ECA),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '• What is Python and why is it popular?\n• Setting up your Python development environment\n',
+                              style: TextStyle(
+                                color: Color(0xFF8B8B8B),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w600,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '2. Python Fundamentals\n',
+                              style: TextStyle(
+                                color: Color(0xFF414ECA),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '• Understanding basic syntax and data types (numbers,\n  strings, booleans, etc.)\n• Working with variables and operators (arithmetic, comparison, logical)\n',
+                              style: TextStyle(
+                                color: Color(0xFF8B8B8B),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w600,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '3. Control Flow\n',
+                              style: TextStyle(
+                                color: Color(0xFF414ECA),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '• Using conditional statements (if/else) for decision making\n• Implementing loops (for, while) for repetitive tasks\n',
+                              style: TextStyle(
+                                color: Color(0xFF8B8B8B),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w600,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '4. Functions\n',
+                              style: TextStyle(
+                                color: Color(0xFF414ECA),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '• Defining and using functions to organize code\n• Understanding arguments and return values\n',
+                              style: TextStyle(
+                                color: Color(0xFF8B8B8B),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w600,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '5. Data Structures\n',
+                              style: TextStyle(
+                                color: Color(0xFF414ECA),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '• Creating and manipulating lists, tuples, and dictionaries\n',
+                              style: TextStyle(
+                                color: Color(0xFF8B8B8B),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w600,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '6. Input and Output\n',
+                              style: TextStyle(
+                                color: Color(0xFF414ECA),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '• Getting user input using the input() function\n• Formatting and printing output to the console\n',
+                              style: TextStyle(
+                                color: Color(0xFF8B8B8B),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w600,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '7. Debugging Techniques\n',
+                              style: TextStyle(
+                                color: Color(0xFF414ECA),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '• Identifying and fixing errors in your Python code\n',
+                              style: TextStyle(
+                                color: Color(0xFF8B8B8B),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w600,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '8. Additional Topics (Optional)\n',
+                              style: TextStyle(
+                                color: Color(0xFF414ECA),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '• Introduction to file handling (reading and writing to files)\n• Basic modules and packages in Python\n',
+                              style: TextStyle(
+                                color: Color(0xFF8B8B8B),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w600,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '9. Practice and Next Steps\n',
+                              style: TextStyle(
+                                color: Color(0xFF414ECA),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: '• Resources for further learning and practice exercises\n• Exploring potential applications of Python\n',
+                              style: TextStyle(
+                                color: Color(0xFF8B8B8B),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w600,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: 'This training will equip you with the essential building blocks of Python programming. By the end, you\'ll be able to write simple Python programs and have a strong foundation for further learning.',
+                              style: TextStyle(
+                                color: Color(0xFF8B8B8B),
+                                fontSize: 10,
+                                fontFamily: 'Nunito',
+                                fontWeight: FontWeight.w600,
+                                height: 0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
+
+                
               ),
+              
             ),
+
+            
+
+            
+
+            
           ),
         ),
+
+
+        
       ),
     );
   }
